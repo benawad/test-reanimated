eeb9f48f436fb8b3d76e2d76b8282201004b3684
Ben Awad <benawad97@gmail.com>
2024-03-08 21:17:15 -0600

    problem persists

--- App.tsx
+++ App.tsx
@@ -1,10 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
 
 export default function App() {
+  const p = useSharedValue(0);
+  useEffect(() => {
+    p.value = withRepeat(withTiming(1, { duration: 1000 }), -1, true);
+  }, []);
+
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          rotate: `${p.value * 2 * Math.PI}rad`,
+        },
+      ],
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Animated.View
+        style={[
+          {
+            width: 100,
+            height: 100,
+            backgroundColor: "red",
+          },
+          style,
+        ]}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
